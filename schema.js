cbb44becebe488132ac5ea534080e2bf3635f2f3
Dhanush-Kalvium <marella.reddy.s69@kalvium.community>
2025-01-28 14:07:23 +0530

    Completed the assignment

---
+++ schema.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const commentSchema = new mongoose.Schema({
+  username: "String",
+  message: { type: string, required: true },
+  commentAt: { type: Date, default: Date.now() },
+});
+
+const blogSchema = new mongoose.Schema({
+  title: { type: String, required: true, min: 5 },
+  content: { type: String, required: true, min: 50 },
+  author: { type: String },
+  tags: { type: [String] },
+  category: { type: String, default: "General" },
+  likes: { type: [String] },
+  createdAt: { type: Date, default: Date.now() },
+  updateAt: { type: Date },
+  comment: commentSchema,
+});
